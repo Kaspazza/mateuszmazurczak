0eb991e6b851d821f1b685cf19e9318410234d54
kaspazza <kaspazza@gmail.com>
2025-07-03 23:18:37 +0200

    Update footer

--- src/cljs/mateuszmazurczak/ui/footer.cljs
+++ src/cljs/mateuszmazurczak/ui/footer.cljs
@@ -6,10 +6,10 @@
 
 (defn footer
   []
-  (let [title (str "Mateusz Mazurczak © " (cljs-time/year (cljs-time/now)))
-        dark? true]
-    [:footer {:class ["footer footer-center text-base-content p-4"
-                      "bg-neutral"]}
+  (let [dark? false]
+    [:footer
+     {:class
+      ["footer sm:footer-horizontal bg-white text-neutral-content items-center justify-end p-4"]}
      [:aside
       [:div {:class ["flex justify-center space-x-10 relative"]}
        [icons/icon {:path-kw :svg/youtube
@@ -23,6 +23,4 @@
        [icons/icon {:path-kw :svg/github
                     :size 1.5
                     :href "https://github.com/kaspazza"
-                    :dark? dark?}]]
-      [:p {:class ["text-base-200"]}
-       title]]]))
+                    :dark? dark?}]]]]))
